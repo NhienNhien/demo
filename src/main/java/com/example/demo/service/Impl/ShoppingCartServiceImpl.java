68fa189132d55fde1f8fba70f5b487cbc7f322bd
NhienNhien <171120012k8>
2023-11-24 01:48:23 +0700

    update

--- src/main/java/com/example/demo/service/Impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ShoppingCartServiceImpl.java
@@ -2,17 +2,29 @@ package com.example.demo.service.Impl;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
 import com.example.demo.entity.CartItem;
+import com.example.demo.repository.ShoppingCartRepository;
 import com.example.demo.service.ShoppingCartService;
 
 @SessionScope
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService{
+	
+	private ShoppingCartRepository shoppingCartRepository;
+	
+	
+	public ShoppingCartServiceImpl(ShoppingCartRepository shoppingCartRepository, Map<Integer, CartItem> maps) {
+		super();
+		this.shoppingCartRepository = shoppingCartRepository;
+		this.maps = maps;
+	}
+
 	Map<Integer, CartItem> maps = new HashMap<>();
 	@Override
 	public void add(CartItem item) {
@@ -22,23 +34,27 @@ public class ShoppingCartServiceImpl implements ShoppingCartService{
 		}else {
 			cartItem.setSoluong(cartItem.getSoluong()+1);
 		}
+		shoppingCartRepository.save(item);
 	}
 	
 	@Override
 	public void remove(int id) {
 		maps.remove(id);
+		shoppingCartRepository.deleteById(id);
 	}
 	
 	@Override
 	public CartItem update(int computerId, int soluong) {
 		CartItem cartItem = maps.get(computerId);
 		cartItem.setSoluong(soluong);
+		shoppingCartRepository.save(cartItem);
 		return cartItem;
 	}
 	
 	@Override
 	public void clear() {
 		maps.clear();
+		shoppingCartRepository.deleteAll();
 	}
 	
 	@Override
